8e048c5ef01f520d34fceb9b62e8cc7b289f75b5
AnnMary Mathew <amm@AnnMarys-MacBook-Pro.local>
2026-01-08 10:07:51 -0500

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,9 @@ export default defineConfig({
       'localhost',
       '127.0.0.1',
       '.sunship.space',
-      '8077ddee1e26.ngrok-free.app'
+      '8077ddee1e26.ngrok-free.app',
+      'app.eezz.ad'
+
     ]
   },
   server: {
